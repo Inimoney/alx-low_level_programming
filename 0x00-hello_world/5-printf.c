6f33052fdddaf8d8663872e95c11d317a530f838
Inimoney <omotolar15@gmail.com>
2023-09-08 15:17:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -A text print followed by a new line using printf function
+ * main - A text print followed by a new line using printf function
  * Return: 0 (true)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art, \n")
 	return (0);
 }
