e920ec3c2fc1dc8caa4dc91446028879e031f950
Inimoney <omotolar15@gmail.com>
2023-09-07 21:33:17 -0700

    helloputs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing of c program text with puts function
+ * Return: 0 (when it is true)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
