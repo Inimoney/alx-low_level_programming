b0a1cf8ca658daacae9f9079a16a12a3673ee756
Inimoney <omotolar15@gmail.com>
2023-09-11 16:13:57 -0700

    succession combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,27 +3,31 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Entry point 
  * Return: Always 0
  */
 
 int main(void)
 
 {
-	int d, p;
-
+	int d, p, q;
+	
 	for (d = '0'; d < '9'; d++)
 	{
 	for (p = d + 1; p <= '9'; p++)
 	{
-	if (p != d)
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
 	{
 	putchar(d);
 	putchar(p);
-	if (d == '8' && p == '9')
+	putchar(q);
+	if (d == '7' && p == '8')
 	continue;
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
