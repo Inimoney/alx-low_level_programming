bad46b16e61f04dab69ba07be46707ffcdf2efa2
Inimoney <omotolar15@gmail.com>
2023-09-12 16:07:14 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Alphabets in lowercase with a new line
+ * Return: 0 (true)
+ */
+
+void print_alphabet(void)
+{
+char c='a';
+
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
