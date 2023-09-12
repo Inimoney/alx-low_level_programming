eeaf613d92c877417e31c2f8bae4a4bd3870f7a7
Inimoney <omotolar15@gmail.com>
2023-09-12 15:48:12 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints putchar with a new line
+ * Return: 0 (true)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return(0);
+}
