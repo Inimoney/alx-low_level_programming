07cdd1137d29f67b10c86b72d712dee96a584fdf
Inimoney <omotolar15@gmail.com>
2023-09-18 16:58:33 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _strlen - A function that returns the length of a string
+ * @s: string to be returned 
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int string_length = 0;
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
