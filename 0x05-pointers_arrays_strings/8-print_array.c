47bc2304610ffbcda61150bb60ed951e334b14df
Inimoney <omotolar15@gmail.com>
2023-09-19 16:44:55 -0700

    arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_array - A function that prints n elements oa an arrayof integers
  * @a: This is the input array
  * @n: This is the length of the array
+ * Return: void
  */
 
 void print_array(int *a, int n)
@@ -13,11 +14,11 @@ void print_array(int *a, int n)
 	for (index_of_array = 0; index_of_array < n; index_of_array++)
 	{
 		printf("%d", a[index_of_array]);
-		if (index_of_array != (n - 1))
+		if (index_of_array < n - 1)
 		{
 			printf(",");
 		}
-		putchar('\n');
+		printf("\n");
 	}
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,4 +14,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 int _atoi(char *s);
+
 #endif
