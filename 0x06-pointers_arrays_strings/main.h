0652e4a5249fc62841f18321c27a7bbbcd65b42d
Inimoney <omotolar15@gmail.com>
2023-09-22 18:09:54 -0700

    rotate string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <string.h>
+#include <stdio.h>
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,5 +11,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 int _putchar(char c);
 char *leet(char *);
-
+char *rot13(char *);
 #endif
